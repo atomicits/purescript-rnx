5377f6b1af155aa8065b9d3dacdb4f3a830c1b1d
Sudhir Kumar <s@atomicits.com>
2016-11-08 13:27:45 +0530

    doc string added

--- src/RNX.purs
+++ src/RNX.purs
@@ -17,16 +17,19 @@ import Signal.Channel (CHANNEL, Channel, channel, subscribe, send)
 import RNX.Components
 
 
+-- | AppKey : Used to define the application name which will be registered with React Native
 type AppKey = String
 
 
+-- | Used to render the application
 foreign import render :: forall a eff. Fn3 (a -> Eff eff Unit) (a -> a) (Element a) (Element a)
 
--- registerComponent
+
+-- | Register the component
 foreign import registerComponent :: forall a e. AppKey -> a ->  Eff e Unit
 
--- Types
 
+-- | Config state action eff. This is the application configuration type
 type Config state action eff =
   { update :: Update state action eff
   , view :: state -> Element action
@@ -34,17 +37,23 @@ type Config state action eff =
   , inputs :: Array (Signal action)
   }
 
+
+-- | Type of the update function
 type Update state action eff = action -> state -> EffModel state action eff
 
 
+-- | Return type of the update function
 type EffModel state action eff =
   { state :: state
-  , effects :: Array (Aff (CoreEffects eff) action) }
+  , effects :: Array (Aff (CoreEffects eff) action)
+  }
 
 
+-- | Type of the core effects of RNX
 type CoreEffects eff = (channel :: CHANNEL, err :: EXCEPTION | eff)
 
 
+-- | App type
 type App state action =
   { html :: Signal (Element action)
   , state :: Signal state
@@ -52,19 +61,21 @@ type App state action =
   }
 
 
--- functions
-
 -- | Create an `Update` function from a simple step function.
 fromSimple :: forall s a eff. (a -> s -> s) -> Update s a eff
 fromSimple update = \action state -> noEffects $ update action state
+
 
 -- | Create an `EffModel` with no effects from a given state.
 noEffects :: forall state action eff. state -> EffModel state action eff
 noEffects state = { state: state, effects: [] }
 
+
+-- | Create an `EffModel` with the given state and effects
 onlyEffects :: forall state action eff.
                state -> Array (Aff (CoreEffects eff) action) -> EffModel state action eff
 onlyEffects state effects = { state: state, effects: effects }
+
 
 -- | Map over the state of an `EffModel`.
 mapState :: forall sa sb a e. (sa -> sb) -> EffModel sa a e -> EffModel sb a e
@@ -77,8 +88,7 @@ mapEffects action effmodel =
   { state: effmodel.state, effects: map (map action) effmodel.effects }
 
 
----- start function
-
+-- | Start the application with the given configuration
 start :: forall state action eff.
          Config state action eff ->
          Eff (CoreEffects eff) (App state action)
