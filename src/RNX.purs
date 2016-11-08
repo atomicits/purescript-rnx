59d10649d251a3281aa7cefbdd7215773c1fe2a4
Sudhir Kumar <s@atomicits.com>
2016-11-08 13:29:59 +0530

    ps 0.91 code removed

--- src/RNX.purs
+++ src/RNX.purs
@@ -2,19 +2,18 @@ module RNX where
 
 import Prelude
 import Control.Monad.Aff (Aff, launchAff, later)
---import Control.Monad.Aff.Unsafe (unsafeInterleaveAff)
 import Control.Monad.Aff.Unsafe (unsafeCoerceAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.Foldable (foldl, sequence_)
-import Data.Function.Uncurried (Fn2, Fn3, runFn2, runFn3)
+import Data.Function.Uncurried (Fn3, runFn3)
 import Data.List (List(Nil), singleton, (:), reverse, fromFoldable)
 import Data.Maybe (fromJust)
 import Partial.Unsafe (unsafePartial)
 import Signal (Signal, (~>), mergeMany, foldp, runSignal)
 import Signal.Channel (CHANNEL, Channel, channel, subscribe, send)
-import RNX.Components
+import RNX.Components (Element)
 
 
 -- | AppKey : Used to define the application name which will be registered with React Native
